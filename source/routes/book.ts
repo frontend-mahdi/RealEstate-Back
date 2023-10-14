8f23b029816758506a5f6c1d6db56bcc5b81fa15
mahdi <mahdi.313.ab@gmail.com>
2023-10-14 18:33:57 +0330

    first commit

---
+++ source/routes/book.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import controller from '../controllers/book';
+
+const router = express.Router();
+
+router.post('/create/book', controller.createBook);
+router.get('/get/books', controller.getAllBooks);
+router.get('/test', controller.getTest);
+
+export = router;
