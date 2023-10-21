585468e36de575a81437a01ce9faa96160683b1f
mahdi <mahdi.313.ab@gmail.com>
2023-10-21 15:03:42 +0330

    get all houses api done

--- source/controllers/house.ts
+++ source/controllers/house.ts
@@ -26,5 +26,25 @@ const createHouse = async (req: Request, res: Response, next: NextFunction) => {
     });
   }
 };
-
-export default { createHouse };
+const getAllHouses = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  House.find()
+    .exec()
+    .then((houses) => {
+      return res.status(200).json({
+        houses: houses,
+        count: houses.length,
+      });
+    })
+    .catch((err) => {
+      const error = err as Error;
+      return res.status(500).json({
+        message: error.message,
+        error,
+      });
+    });
+};
+export default { createHouse, getAllHouses };
--- source/routes/owner.ts
+++ source/routes/owner.ts
@@ -4,7 +4,7 @@ import controller from "../controllers/house";
 const router = express.Router();
 
 router.post("/house", controller.createHouse);
-// router.get("/get/books", controller.getAllBooks);
+router.get("/houses", controller.getAllHouses);
 // router.get("/test", controller.getTest);
 
 export = router;
